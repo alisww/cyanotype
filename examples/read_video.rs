b885f36fe6b642149aad9819fe1bf58076ee63b5
Allie Signet <allie@cat-girl.gay>
2022-02-12 16:53:13 -0300

    Revert "change to providing raw video frames"
    This reverts commit 6856dbbd29588fb760ef453466720919ffba0615.

--- examples/read_video.rs
+++ examples/read_video.rs
@@ -28,7 +28,7 @@ async fn main() {
                 // if let Ok(_p) = packet {
                 idx += 1;
                 // }
-                println!("{:?}",packet.pts());
+                println!("{:?}",packet.time);
             },
             else => break
         }
--- src/streams/video.rs
+++ src/streams/video.rs
@@ -4,6 +4,7 @@ use super::*;
 use crate::*;
 use async_trait::async_trait;
 
+use ac_ffmpeg::codec::video::{PixelFormat, VideoDecoder, VideoFrameScaler};
 use ac_ffmpeg::codec::{CodecParameters, Decoder};
 use ac_ffmpeg::format::stream::Stream as FFmpegStream;
 use ac_ffmpeg::packet::Packet as FFmpegPacket;
@@ -13,11 +14,18 @@ use async_broadcast::{
     Receiver as BroadcastReceiver, Sender as BroadcastSender,
 };
 use futures::stream::Stream;
-
+use image::RgbaImage;
 use std::collections::HashMap;
 use std::pin::Pin;
+use std::str::FromStr;
+use std::time::Duration;
 
-pub use ac_ffmpeg::codec::video::{PixelFormat, VideoDecoder, VideoFrame, VideoFrameScaler};
+/// A simple video packet, containing the image data of the frame and the time where it should be presented.
+#[derive(Clone)]
+pub struct VideoPacket<T> {
+    pub frame: T,
+    pub time: Duration,
+}
 
 /// A stream adapter over an underlying ffmpeg video decoder, which converts raw frames into [image::RgbaImage].
 pub struct VideoStream {
@@ -29,14 +37,17 @@ pub struct VideoStream {
     extra_data: Option<Vec<u8>>,
     parameters: CodecParameters,
     video_decoder: VideoDecoder,
+    video_transformer: VideoFrameScaler,
+    height: u32,
+    width: u32,
     block: bool,
-    tx: BroadcastSender<VideoFrame>,
-    rx: InactiveBroadcastReceiver<VideoFrame>,
+    tx: BroadcastSender<VideoPacket<RgbaImage>>,
+    rx: InactiveBroadcastReceiver<VideoPacket<RgbaImage>>,
 }
 
 #[async_trait]
 impl PacketStream for VideoStream {
-    type Packet = VideoFrame;
+    type Packet = VideoPacket<RgbaImage>;
 
     fn from_ffmpeg(stream: &FFmpegStream) -> Result<Self> {
         let parameters = stream.codec_parameters();
@@ -44,15 +55,28 @@ impl PacketStream for VideoStream {
 
         let (tx, rx) = broadcast_channel(64);
         let video_decoder = VideoDecoder::from_stream(stream)?.build()?;
-        let _video_params = parameters.as_video_codec_parameters().unwrap();
+        let video_params = parameters.as_video_codec_parameters().unwrap();
+        let (width, height) = (video_params.width(), video_params.height());
+
+        let video_transformer = VideoFrameScaler::builder()
+            .source_pixel_format(video_params.pixel_format())
+            .source_height(height)
+            .source_width(width)
+            .target_height(height)
+            .target_width(width)
+            .target_pixel_format(PixelFormat::from_str("rgba").unwrap())
+            .build()?;
 
         Ok(VideoStream {
+            height: height as u32,
+            width: width as u32,
             metadata: stream.metadata_dict(),
             time_base: stream.time_base(),
             start_time: stream.start_time(),
             duration: stream.duration(),
             frames: stream.frames(),
             block: false,
+            video_transformer,
             video_decoder,
             extra_data,
             parameters,
@@ -117,11 +141,18 @@ impl VideoStream {
         self.block = block;
     }
 
-    pub fn pixel_format(&self) -> PixelFormat {
-        self.parameters
-            .as_video_codec_parameters()
-            .unwrap()
-            .pixel_format()
+    fn decode_frame(
+        &mut self,
+        frame: ac_ffmpeg::codec::video::frame::VideoFrame,
+    ) -> Result<VideoPacket<RgbaImage>> {
+        let frame = self.video_transformer.scale(&frame)?;
+        Ok(VideoPacket {
+            time: Duration::from_nanos(
+                frame.pts().as_nanos().ok_or(CyanotypeError::TimeMissing)? as u64
+            ),
+            frame: RgbaImage::from_raw(self.width, self.height, frame.planes()[0].data().to_vec())
+                .ok_or(CyanotypeError::ImageDecodeError)?,
+        })
     }
 }
 
@@ -130,6 +161,7 @@ impl DecoderStream for VideoStream {
     async fn run(&mut self) -> Result<()> {
         while let Some(frame) = self.video_decoder.take()? {
             if self.block || self.tx.receiver_count() > 0 {
+                let frame = self.decode_frame(frame)?;
                 self.tx
                     .broadcast(frame)
                     .await
